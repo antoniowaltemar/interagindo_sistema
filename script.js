03159c23774db027d29b91dea9ad6a2a999864ff
Waltemar Almeida <antoniowaltemarbjj@gmail.com>
2022-08-13 18:10:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let baixo= document.getElementById('baixo');
+let d= new Date();
+
+
+function clique(){
+    document.getElementById('baixo').innerHTML= `${'O que eu recebi do sistema foi:'+ d}`;
+}
